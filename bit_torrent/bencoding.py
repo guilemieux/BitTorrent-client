d74f6fdb2fb881e2d1747ff4837ecefb37a67f51
Guillaume Lemieux <guilemieux@gmail.com>
2020-01-21 15:49:49 -0500

    Implement bdecoding

--- bit_torrent/bencoding.py
+++ bit_torrent/bencoding.py
@@ -1,8 +1,9 @@
 """
 Encoding and decoding
 """
+from typing import Tuple
 
-def bencode(data:object):
+def bencode(data: object) -> str:
     if isinstance(data, str):
         return bencode_string(data)
     elif isinstance(data, int):
@@ -15,26 +16,64 @@ def bencode(data:object):
         raise Exception("Type " + str(type(data)) + " can't be bencoded")
     
 
-def bdecode(data):
-    # TODO
-    pass
+def bdecode(encoding: str):
+    result, pos = bdecode_be(encoding, 0)
+    if pos != len(encoding):
+        raise Exception('invalid bencoding (data after valid prefix)')
+    return result
 
-def bencode_string(s:str):
+def read(filename: str):
+    with open(filename, 'rb') as file:
+        return bdecode(file.read())
+
+def bencode_string(s:str) -> str:
     return str(len(s)) + ':' + s
 
-def bencode_int(n:int):
+def bencode_int(n:int) -> str:
     return 'i' + str(n) + 'e'
 
-def bencode_list(l:list):
+def bencode_list(l:list) -> str:
     bencoding = 'l'
     for element in l:
         bencoding += bencode(element)
     return bencoding + 'e'
 
-def bencode_dict(d:dict):
+def bencode_dict(d:dict) -> str:
     bencoding = 'd'
     for key in d:
         bencoding += bencode(key) + bencode(d[key])
     return bencoding + 'e'
 
-print(bencode(lambda x: x**2))
+def bdecode_be(s:str, pos:int) -> Tuple[str, int]:
+    if chr(s[pos]).isdigit(): return bdecode_string(s, pos)
+    elif s[pos] == ord('i'): return bdecode_int(s, pos)
+    elif s[pos] == ord('l'): return bdecode_list(s, pos)
+    elif s[pos] == ord('d'): return bdecode_dict(s, pos)
+    else: raise Exception('Invalid bencoding')
+
+def bdecode_string(s:str, pos:int) -> Tuple[str, int]:
+    colon_pos = pos + s[pos:].index(b':')
+    n = int(s[pos:colon_pos])
+    start_pos = colon_pos + 1
+    end_pos = start_pos + n
+    return s[start_pos:end_pos], end_pos
+
+def bdecode_int(s:str, pos:int) -> Tuple[int, int]:
+    e_pos = pos + s[pos:].index(b'e')
+    return int(s[pos + 1:e_pos]), e_pos + 1
+
+def bdecode_list(s:str, pos:int) -> Tuple[list, int]:
+    l = []
+    pos += 1
+    while s[pos] != ord('e'):
+        element, pos = bdecode_be(s, pos)
+        l.append(element)
+    return l, pos + 1
+
+def bdecode_dict(s:str, pos:int) -> Tuple[dict, int]:
+    d = {}
+    pos += 1
+    while s[pos] != ord('e'):
+        key, pos = bdecode_be(s, pos)
+        d[key], pos = bdecode_be(s, pos)
+    return d, pos + 1
